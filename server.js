ba42f9019f936fd884cbacec33c531c48e7ba4f5
Ray He <devrayhe@gmail.com>
2021-11-05 21:12:16 +1100

    Backend
    Basic express backend structure -
    - Serving static files: index.html and notes.html
    - Server listen on PORT 3002

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 const express = require('express');
 const path = require('path');
+const data = require('./db/db.json');
+
+
+const PORT = process.env.port || 3002;
+const app = express();
+
+// Middleware for unlencoded data
+app.use(express.urlencoded({ extend: true}));
+// Middleware for parsing application/json
+app.use(express.json());
+
+app.use(express.static('public'))
+
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.listen(PORT, () => 
+  console.log(`App listening at http://localhost:${PORT}`)
+);
