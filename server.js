3b1008f24f0355163050fa29d1953695b5d797cb
Ray He <devrayhe@gmail.com>
2021-11-06 06:58:33 +1100

    Add a note
    Added POST request '/api/notes' to add new notes to database
    Added uniqid to generate unique id for each note

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const uniqid = require('uniqid')
 const notes = require('./db/db.json');
 
 
@@ -14,10 +16,55 @@ app.use(express.json());
 app.use(express.static('public'))
 
 
-// app.post();
+// Post request to add a note
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to post a note`);
+
+  const {title, text} = req.body;
+  const id = uniqid();
+  console.info(`${req.body}  ${title}  ${text}`);
+
+  const newNote = {
+    title,
+    text,
+    id,
+  }
+
+  // Obtain exisiting notes
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+
+      // Add a new note
+      parsedNotes.push(newNote);
+
+      fs.writeFile(
+        './db/db.json', 
+        JSON.stringify(parsedNotes, null, 2),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated notes!')
+      )
+    }
+  });
+
+  const response = {
+    status: 'success',
+    body: newNote,
+  }
+
+  console.log(response);
+  res.json(response);
+});
+
 
 // app.delete();
 
+// Get request to retrive current notes
 app.get('/api/notes', (req, res) => {
   // Log request to the terminal
   console.info(`${req.method} request received to get notes`);
@@ -25,6 +72,7 @@ app.get('/api/notes', (req, res) => {
   return res.json(notes);
 });
 
+// Get request to retrive nots.html file
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
 });
