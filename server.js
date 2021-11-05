6678a37c3e382cf6e63a58c9718d03de19c1d53f
Ray He <devrayhe@gmail.com>
2021-11-06 05:54:15 +1100

    Update server.js
    Added GET request '/api/notes' to receive and update current notes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const data = require('./db/db.json');
+const notes = require('./db/db.json');
 
 
 const PORT = process.env.port || 3002;
@@ -14,14 +14,25 @@ app.use(express.json());
 app.use(express.static('public'))
 
 
-app.get('/notes', (req, res) =>
+// app.post();
+
+// app.delete();
+
+app.get('/api/notes', (req, res) => {
+  // Log request to the terminal
+  console.info(`${req.method} request received to get notes`);
+
+  return res.json(notes);
+});
+
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+});
 
-app.get('*', (req, res) =>
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
-);
+});
 
-app.listen(PORT, () => 
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.info(`App listening at http://localhost:${PORT}`)
+});
